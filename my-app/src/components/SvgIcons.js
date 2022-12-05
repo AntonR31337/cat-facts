96e50be62b6a28083df54f1da130dd28a76735a1
Anton Ryabtsev <anton_ryabcev@mail.ru>
2022-12-05 22:19:05 +0300

    fixed some bugs

--- my-app/src/components/SvgIcons.js
+++ my-app/src/components/SvgIcons.js
@@ -3,8 +3,8 @@ import { Svg } from "./styledComponentsStyles";
 function SvgIcons({ id, color }) {
     switch (id) {
         case 'like':
-            return <Svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill={color} class="bi bi-heart-fill" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+            return <Svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill={color} viewBox="0 0 16 16">
+            <path d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
           </Svg>
         case 'close':
             return <Svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" viewBox="0 0 16 16">
